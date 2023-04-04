2374905d60c3be0543593d10ac6dab06d621ac6c
LeonardoHui <leonardo.hui.learning@gmail.com>
2023-04-03 23:57:40 -0300

    feat: add request to OpenAi Embeddings

--- main.go
+++ main.go
@@ -1,95 +1,111 @@
 package main
 
 import (
-	"context"
+	"errors"
 	"fmt"
-	"github.com/openai/api"
 	"log"
-	"strings"
+	"math"
+	"tests-selector/openai"
 )
 
 func main() {
-	// Set up OpenAI API credentials.
-	apiKey := "YOUR_API_KEY"
-	client, err := api.NewClient(apiKey)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	// Load test cases from file.
 	testCases := loadTestCasesFromFile("test_cases.txt")
 
 	// Generate embeddings for the test cases.
-	testCaseEmbeddings := make([][]float32, len(testCases))
+	testCaseEmbeddings := make([][][]float32, len(testCases))
 	for i, testCase := range testCases {
 		// Generate embedding for the test case using OpenAI API.
-		resp, err := client.Completions(context.Background(), &api.CompletionRequest{
-			Model: "text-davinci-002",
-			Prompt: testCase,
-			MaxTokens: 64,
-			N: 1,
-			Temperature: 0.0,
+		resp, err := openai.Embeddings(openai.EmbeddingsRequest{
+			Model: "text-embedding-ada-002",
+			Input: testCase,
 		})
 		if err != nil {
 			log.Fatal(err)
 		}
-		embedding := resp.Choices[0].Text
-		embedding = strings.TrimSpace(embedding)
-		embeddingValues := strings.Split(embedding, ",")
-		testCaseEmbeddings[i] = make([]float32, len(embeddingValues))
-		for j, val := range embeddingValues {
-			fmt.Sscanf(val, "%f", &testCaseEmbeddings[i][j])
+
+		testCaseEmbeddings[i] = make([][]float32, len(resp.Data))
+		if len(resp.Data) != len(testCase) {
+			log.Fatal("Error response size doesnt match")
+		}
+		for _, dt := range resp.Data {
+			testCaseEmbeddings[i][dt.Index] = make([]float32, len(dt.Embedding))
+			testCaseEmbeddings[i][dt.Index] = dt.Embedding
 		}
 	}
 
 	// Compute cosine similarity between the PR description and each test case.
 	prDescription := "Fix bug in login flow"
-	prDescriptionEmbedding, err := generateEmbedding(client, prDescription)
+	prDescriptionEmbedding, err := generateEmbedding(prDescription)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	for i, testCase := range testCases {
-		cosineSim := cosineSimilarity(prDescriptionEmbedding, testCaseEmbeddings[i])
-		fmt.Printf("Test case %d: %s, cosine similarity: %f\n", i+1, testCase, cosineSim)
+		for j, t := range testCase {
+			cosineSim := cosineSimilarity(prDescriptionEmbedding, testCaseEmbeddings[i][j])
+			fmt.Printf("Test case %d: %s, cosine similarity: %f\n", i+1, t, cosineSim)
+		}
 	}
 }
 
 // Load test cases from a file.
-func loadTestCasesFromFile(filename string) []string {
+func loadTestCasesFromFile(filename string) [][]string {
 	// TODO: Implement file loading.
-	return []string{
-		"Test case 1",
-		"Test case 2",
-		"Test case 3",
+	return [][]string{
+		{"Testing login flow"},
+		{"Testing purchase flow"},
 	}
 }
 
 // Generate an embedding for a text prompt using OpenAI API.
-func generateEmbedding(client *api.Client, prompt string) ([]float32, error) {
-	resp, err := client.Completions(context.Background(), &api.CompletionRequest{
-		Model: "text-davinci-002",
-		Prompt: prompt,
-		MaxTokens: 64,
-		N: 1,
-		Temperature: 0.0,
+func generateEmbedding(prompt string) ([]float32, error) {
+	resp, err := openai.Embeddings(openai.EmbeddingsRequest{
+		Model: "text-embedding-ada-002", //text-embedding-ada-002
+		Input: []string{prompt},
 	})
 	if err != nil {
 		return nil, err
 	}
-	embedding := resp.Choices[0].Text
-	embedding = strings.TrimSpace(embedding)
-	embeddingValues := strings.Split(embedding, ",")
-	embeddingFloats := make([]float32, len(embeddingValues))
-	for i, val := range embeddingValues {
-		fmt.Sscanf(val, "%f", &embeddingFloats[i])
+
+	if len(resp.Data) != 1 {
+		return nil, errors.New("Error response size doesn't match")
 	}
+
+	embeddingFloats := resp.Data[0].Embedding
 	return embeddingFloats, nil
 }
 
 // Compute cosine similarity between two vectors.
+// Range  ranges from -1 to 1,
+// with -1 indicating that two vectors are completely dissimilar,
+// 0 indicating that they are orthogonal (i.e., have no relationship),
+// and 1 indicating that they are identical or very similar.
 func cosineSimilarity(vec1, vec2 []float32) float32 {
-	var dotProduct float32
-	var mag1, mag2 float32
-	for i := range vec1 {
+	// Calculate the dot product of the two vectors
+	dotProduct := float32(0)
+	for i := 0; i < len(vec1); i++ {
 		dotProduct += vec1[i] * vec2[i]
 	}
+
+	// Calculate the magnitude of the first vector
+	vec1Magnitude := float32(0)
+	for _, v := range vec1 {
+		vec1Magnitude += v * v
+	}
+	vec1Magnitude = float32(math.Sqrt(float64(vec1Magnitude)))
+
+	// Calculate the magnitude of the second vector
+	vec2Magnitude := float32(0)
+	for _, v := range vec2 {
+		vec2Magnitude += v * v
+	}
+	vec2Magnitude = float32(math.Sqrt(float64(vec2Magnitude)))
+
+	// Calculate the cosine similarity of the two vectors
+	if vec1Magnitude == 0 || vec2Magnitude == 0 {
+		return 0
+	}
+	return dotProduct / (vec1Magnitude * vec2Magnitude)
+}
---
+++ openai/embeddings.go
@@ -0,0 +1,74 @@
+package openai
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type EmbeddingsRequest struct {
+	Model string   `json:"model"`
+	Input []string `json:"input"`
+	User  string   `json:"user"`
+}
+
+type EmbeddingsResponse struct {
+	Object string `json:"object"`
+	Data   []data `json:"data"`
+	Model  string `json:"model"`
+	Usage  usage  `json:"usage"`
+}
+
+type data struct {
+	Object    string    `json:"object"`
+	Embedding []float32 `json:"embedding"`
+	Index     int       `json:"index"`
+}
+
+type usage struct {
+	PromptTokens int `json:"prompt_tokens"`
+	TotalTokens  int `json:"total_tokens"`
+}
+
+type EmbeddingsErrorResponse struct {
+	Error struct {
+		Message string `json:"message"`
+		Type    string `json:"type"`
+		Param   string `json:"param"`
+		Code    int    `json:"code"`
+	} `json:"error"`
+}
+
+func Embeddings(req EmbeddingsRequest) (resp EmbeddingsResponse, err error) {
+
+	reqBytes, _ := json.Marshal(req)
+
+	r, err := http.NewRequest("POST", "https://api.openai.com/v1/embeddings", bytes.NewBuffer(reqBytes))
+	r.Header.Add("Content-Type", "application/json")
+	r.Header.Add("Authorization", "Bearer YOUR-TOKEN")
+
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	if res.StatusCode != http.StatusOK {
+		fmt.Println("Error:", string(body))
+		err = errors.New("Request returned != 200")
+		return
+	}
+
+	err = json.Unmarshal(body, &resp)
+	return
+}
